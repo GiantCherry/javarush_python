936a6b1231fefdca6477373042270b7546b1fc59
GiantCherry <lunev1981@mail.ru>
2025-04-27 22:55:51 +0300

    Task 8.12

---
+++ src/ru/javarush/python/core/level08/task12/task12.py
@@ -0,0 +1,26 @@
+# Длительность работы.
+
+# Напишите программу, которая создает декоратор для измерения времени выполнения функции.
+# Программа должна:
+# Определить декоратор time_decorator, который измеряет и выводит время выполнения функции.
+# Применить декоратор к функции compute_square(n), которая вычисляет квадрат числа и имитирует задержку с помощью time.sleep().
+# Вызвать функцию compute_square(n).
+
+# Напишите тут ваш код
+import time
+
+def time_decorator(func):
+    def wrapper(n):
+        start_time = time.time()
+        result = func(n)
+        end_time = time.time()
+        print(f"Время выполнения: {end_time - start_time} секунд")
+        return result
+    return wrapper
+
+@time_decorator
+def compute_square(n):
+    time.sleep(2)  # имитация задержки
+    return n * n
+
+compute_square(5)
