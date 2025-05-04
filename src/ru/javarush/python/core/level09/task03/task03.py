03ad47db94ebec4c9cf3241cb4f744bf7c8d2b9d
GiantCherry <lunev1981@mail.ru>
2025-05-04 18:09:32 +0300

    Task 9.3

---
+++ src/ru/javarush/python/core/level09/task03/task03.py
@@ -0,0 +1,17 @@
+# Прямоугольники.
+
+# Создайте класс Rectangle с конструктором, который принимает параметры width и height.
+# Добавьте метод area(), который возвращает площадь прямоугольника.
+# Создайте объект этого класса и вычислите его площадь.
+
+# Напишите тут ваш код
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+rect = Rectangle(4, 5)
+print(rect.area())
