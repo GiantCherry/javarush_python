ece3257697c56fbdd3075b0a1b25e278f5b48aed
GiantCherry <lunev1981@mail.ru>
2025-10-10 19:02:04 +0300

    Python. Task 23.30. Additions

--- src/ru/javarush/python/core/level23/task05/solution.py
+++ src/ru/javarush/python/core/level23/task05/solution.py
@@ -3,10 +3,19 @@ from bs4 import BeautifulSoup
 
 # URL для загрузки
 url = 'https://example.com'
+url = 'https://www.ozon.ru/seller/santegra-1037570/?miniapp=seller_1037570' # ИП Михайлов
 
 # Выполнение HTTP-запроса для получения HTML-кода страницы
 response = requests.get(url)
 
+# Parsing Ozon
+response.encoding = 'utf-8'
+print(response.encoding)
+soup = BeautifulSoup(response.text, 'html.parser')
+print(soup.prettify())
+# Parsing Ozon
+
+'''
 # Парсинг HTML-кода с помощью BeautifulSoup
 soup = BeautifulSoup(response.text, 'html.parser')
 
@@ -15,3 +24,4 @@ title = soup.title.text
 
 # Вывод заголовка страницы
 print('Title: ', title)
+'''
