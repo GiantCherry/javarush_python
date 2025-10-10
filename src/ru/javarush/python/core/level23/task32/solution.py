da1da88858932847674a5c136f3a7f0342a2c14b
GiantCherry <lunev1981@mail.ru>
2025-10-10 20:05:11 +0300

    Python. Task 23.32-2

--- src/ru/javarush/python/core/level23/task32/solution.py
+++ src/ru/javarush/python/core/level23/task32/solution.py
@@ -29,3 +29,29 @@ fruits = [fruit for fruit in fruits_in_table if fruit in fruits_in_ul]
 
 # Выводим результат
 print(fruits)
+
+'''
+from bs4 import BeautifulSoup
+
+# Путь к HTML-файлу, который будем анализировать
+file_path = "example.html"  # Укажите путь к вашему HTML-файлу
+
+# Открываем HTML-файл и читаем его содержимое
+with open(file_path, 'r', encoding='utf-8') as file:
+    html_content = file.read()
+
+# Создаем объект BeautifulSoup
+soup = BeautifulSoup(html_content, 'html.parser')
+
+# Извлекаем данные из таблицы
+table_fruits = {row.find('td').text for row in soup.find_all('tr')[1:]}
+
+# Извлекаем данные из списка
+list_fruits = {li.text for li in soup.find_all('ul')[0].find_all('li')}
+
+# Сравниваем и получаем общее множество
+common_fruits = list(table_fruits & list_fruits)
+
+# Выводим результат
+print(common_fruits)
+'''
