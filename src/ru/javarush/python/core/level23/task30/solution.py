1d32cde17b910d09555b71466fa023fa4ada85be
GiantCherry <lunev1981@mail.ru>
2025-10-10 18:59:57 +0300

    Python. Task 23.30

---
+++ src/ru/javarush/python/core/level23/task30/solution.py
@@ -0,0 +1,20 @@
+from bs4 import BeautifulSoup
+
+# Путь к HTML-файлу, который будем анализировать
+file_path = "example.html"  # Укажите путь к вашему HTML-файлу
+
+# Открываем HTML-файл и читаем его содержимое
+with open(file_path, 'r', encoding='utf-8') as file:
+    html_content = file.read()
+
+# Создание объекта BeautifulSoup
+soup = BeautifulSoup(html_content, 'html.parser')
+
+# Извлечение элементов списка <li>
+li_elements = soup.find_all('li')
+
+# Формирование списка Python из текста элементов
+items = [item.getText() for item in li_elements if item]
+
+# Вывод результата
+print(items)
